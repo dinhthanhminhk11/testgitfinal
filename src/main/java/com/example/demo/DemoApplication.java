30710322495c4c81a1a51ddc09b2d008226095d4
minhthanhminh <dinhthanhminhk11@gmail.com>
2022-08-18 23:08:28 +0700

    test comic

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("spring boot");
+		System.out.println("dinh thanh minh")
 	}
 
 }
